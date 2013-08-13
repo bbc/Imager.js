4e51334cf8a4336696b6aed179220ec7b8a87696
Mark McDonnell <mark.mcdx@gmail.com>
2013-08-13 09:13:27 +0100

    Update code to use BBC formatting and style guide...
    - Correct formatting to 4 spaces
    - Removed unnecessary comments (code should be speak for itself, if it doesn't: refactor)
    - No point specifying `undefined` as a return value
    - Ensure IIFE executing parenthesis are on the inside: (function(){}())

--- Demo/Assets/Scripts/Imager.js
+++ Demo/Assets/Scripts/Imager.js
@@ -1,187 +1,155 @@
 (function (window, document) {
 
-  'use strict';
+    'use strict';
 
-  var $, Imager;
+    var $, Imager;
 
-  // requestAnimationFrame shim.
-  window.requestAnimationFrame = window.requestAnimationFrame
-  || window.mozRequestAnimationFrame
-  || window.webkitRequestAnimationFrame
-  || function (callback) {
-    window.setTimeout(callback, 1000 / 60);
-  };
-
-
-  // jQuery substitute, if necessary.
-  $ = (function (dollar) {
-
-    if (dollar) {
-      return dollar;
-    }
-
-    return function (selector) {
-      return Array.prototype.slice.call(document.querySelectorAll(selector));
+    window.requestAnimationFrame = 
+    window.requestAnimationFrame || 
+    window.mozRequestAnimationFrame || 
+    window.webkitRequestAnimationFrame || 
+    function (callback) {
+        window.setTimeout(callback, 1000 / 60);
     };
-  })(window.$);
 
 
-  // Construct a new Imager instance, passing an optional configuration object,
-  // e.g.
-  //
-  //   {
-  //     // Available widths for your images.
-  //     availableWidths: [Number]
-  //
-  //     // Selector your div placeholders share.
-  //     selector: '',
-  //
-  //     // Class name to give your resizable images.
-  //     className: '',
-  //
-  //     // Regular expression to match against your image endpoint's naming
-  //     // conventions (e.g. http://yourserver.com/image/horse/400)
-  //     regex: RegExp
-  //   }
-  //
-  // @param  {object}
-  // @return {Imager}
-  window.Imager = Imager = function (opts) {
+    $ = (function (dollar) {
+        if (dollar) {
+            return dollar;
+        }
 
-    var self = this;
-    opts = opts || {};
-
-    this.availableWidths = opts.availableWidths || [ 96, 130, 165, 200, 235,
-    270, 304, 340, 375, 410, 445, 485, 520, 555, 590, 625, 660, 695, 736 ];
-
-    this.selector = opts.selector || '.delayed-image-load';
-    this.className = '.' + (opts.className || 'image-replace').replace(/^\.+/, '.');
-    this.regex = opts.regex || /^(.+\/)\d+$/i;
-
-    this.gif = document.createElement('img');
-    this.gif.src = 'data:image/gif;base64,R0lGODlhEAAJAIAAAP///wAAACH5BAEAAAAALAAAAAAQAAkAAAIKhI+py+0Po5yUFQA7';
-    this.gif.className = this.className.replace(/^[#.]/, '');
-
-    this.divs = $(this.selector);
-    this.changeDivsToEmptyImages();
-
-    this.cache = {};
-
-    window.requestAnimationFrame(function () {
-      self.init();
-    });
-  };
+        return function (selector) {
+            return Array.prototype.slice.call(document.querySelectorAll(selector));
+        };
+    }(window.$));
 
 
-  // At instantiation, assign a resize listener, responsible for triggering an
-  // Imager resize callback.
-  //
-  // @return {undefined}
-  Imager.prototype.init = function () {
+    /*
+        Construct a new Imager instance, passing an optional configuration object.
 
-    var self = this;
+        Example usage:
 
-    this.initialized = true;
-    this.resizeImages();
+            {
+                // Available widths for your images
+                availableWidths: [Number]
 
-    window.addEventListener('resize', function () {
-      self.resizeImages();
-    }, false);
-  };
+                // Selector to be used to locate your div placeholders
+                selector: '',
 
+                // Class name to give your resizable images.
+                className: '',
 
-  // At instantiation, replace all `image replacer` div elements with empty
-  // images.
-  //
-  // @return {undefined}
-  Imager.prototype.changeDivsToEmptyImages = function () {
+                // Regular expression to match against your image endpoint's naming conventions 
+                // e.g. http://yourserver.com/image/horse/400
+                regex: RegExp
+            }
 
-    var divs = this.divs,
-        i = divs.length,
-        gif;
+        @param {object} configuration settings
+        @return {object} instance of Imager
+     */
+    window.Imager = Imager = function (opts) {
+        var self = this;
+            opts = opts || {};
 
-    while (i--) {
-      gif = this.gif.cloneNode(false);
-      gif.width = divs[i].getAttribute('data-width');
-      gif.setAttribute('data-src', divs[i].getAttribute('data-src'));
-      divs[i].parentNode.replaceChild(gif, divs[i]);
-    }
+        this.availableWidths = opts.availableWidths || [96, 130, 165, 200, 235, 270, 304, 340, 375, 410, 445, 485, 520, 555, 590, 625, 660, 695, 736];
+        this.selector        = opts.selector || '.delayed-image-load';
+        this.className       = '.' + (opts.className || 'image-replace').replace(/^\.+/, '.');
+        this.regex           = opts.regex || /^(.+\/)\d+$/i;
+        this.gif             = document.createElement('img');
+        this.gif.src         = 'data:image/gif;base64,R0lGODlhEAAJAIAAAP///wAAACH5BAEAAAAALAAAAAAQAAkAAAIKhI+py+0Po5yUFQA7';
+        this.gif.className   = this.className.replace(/^[#.]/, '');
+        this.divs            = $(this.selector);
+        this.cache           = {};
+        this.changeDivsToEmptyImages();
 
-    if (this.initialized) {
-      this.resizeImages();
-    }
-  };
-
-
-  // Iterate over the images discovered on the document, replacing them with the
-  // appropriate image for a user's device.
-  //
-  // @return {undefined}
-  Imager.prototype.resizeImages = function () {
-
-    var self = this,
-        images = $(this.className),
-        i = images.length;
-
-    if (!this.isResizing) {
-      this.isResizing = true;
-
-      while (i--) {
-        this.placeMaxResolutionImage(images[i]);
-      }
-
-      this.isResizing = false;
-    }
-  };
-
-  // Modify an image object with new properties, tailored to the resolution the
-  // user's device fits.
-  //
-  // A caching mechanism is used when a particular resolution for an image has
-  // already been downloaded.
-  //
-  // @param  {object} image
-  // @return {undefined}
-  Imager.prototype.placeMaxResolutionImage = function (image) {
-
-    var src = this.determineMaxResolution(image),
-        parent = image.parentNode,
-        replacedImage;
-
-    if (this.cache[src]) {
-      replacedImage = this.cache[src].cloneNode(false);
-      replacedImage.width = image.getAttribute('width');
-    } else {
-      replacedImage = image.cloneNode(false);
-      replacedImage.src = src;
-      this.cache[src] = replacedImage;
-    }
-
-    parent.replaceChild(replacedImage, image);
-  };
+        window.requestAnimationFrame(function(){
+            self.init();
+        });
+    };
 
 
-  // Determine the appropriate resolution image to download, based on the width
-  // the image passed in is being viewed at.
-  //
-  // @param  {object} image
-  // @return {string} the path to the image
-  Imager.prototype.determineMaxResolution = function (image) {
+    Imager.prototype.init = function () {
+        var self = this;
 
-    var self = this,
-        src = image.getAttribute('data-src'),
-        width = image.clientWidth,
-        selectedWidth = this.availableWidths[0],
-        i = this.availableWidths.length;
+        this.initialized = true;
+        this.checkImagesNeedReplacing();
 
-    while (i--) {
-      if (width <= this.availableWidths[i]) {
-        selectedWidth = this.availableWidths[i];
-      }
-    }
+        window.addEventListener('resize', function(){
+            self.checkImagesNeedReplacing();
+        }, false);
+    };
 
-    return src.replace(this.regex, function (match, captured) {
-      return captured + selectedWidth;
-    });
-  };
-})(window, document);
+
+    Imager.prototype.changeDivsToEmptyImages = function () {
+        var divs = this.divs,
+            i = divs.length,
+            gif;
+
+        while (i--) {
+            gif = this.gif.cloneNode(false);
+            gif.width = divs[i].getAttribute('data-width');
+            gif.setAttribute('data-src', divs[i].getAttribute('data-src'));
+            divs[i].parentNode.replaceChild(gif, divs[i]);
+        }
+
+        if (this.initialized) {
+            this.checkImagesNeedReplacing();
+        }
+    };
+
+
+    Imager.prototype.checkImagesNeedReplacing = function () {
+        var self = this,
+            images = $(this.className),
+            i = images.length;
+
+        if (!this.isResizing) {
+            this.isResizing = true;
+
+            while (i--) {
+                this.replaceImagesBasedOnScreenDimensions(images[i]);
+            }
+
+            this.isResizing = false;
+        }
+    };
+
+    Imager.prototype.replaceImagesBasedOnScreenDimensions = function (image) {
+        var src = this.determineAppropriateResolution(image),
+            parent = image.parentNode,
+            replacedImage;
+
+        if (this.cache[src]) {
+            replacedImage = this.cache[src].cloneNode(false);
+            replacedImage.width = image.getAttribute('width');
+        } else {
+            replacedImage = image.cloneNode(false);
+            replacedImage.src = src;
+            this.cache[src] = replacedImage;
+        }
+
+        parent.replaceChild(replacedImage, image);
+    };
+
+    Imager.prototype.determineAppropriateResolution = function (image) {
+        var src           = image.getAttribute('data-src'),
+            imagewidth    = image.clientWidth,
+            selectedWidth = this.availableWidths[0],
+            i             = this.availableWidths.length;
+
+        while (i--) {
+            if (imagewidth <= this.availableWidths[i]) {
+                selectedWidth = this.availableWidths[i];
+            }
+        }
+
+        return this.changeImageSrcToUseNewImageDimensions(src, selectedWidth);
+    };
+
+    Imager.prototype.changeImageSrcToUseNewImageDimensions = function (src, selectedWidth) {
+        return src.replace(this.regex, function (match, captured) {
+            return captured + selectedWidth;
+        });
+    };
+
+}(window, document));
