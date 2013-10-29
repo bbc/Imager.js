5ca9e89ce72be3e7f7ed626166f01d0af4298470
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-10-29 15:40:15 +0000

    Testing constructor.

--- test/unit/core.js
+++ test/unit/core.js
@@ -3,8 +3,16 @@
 describe('Imager.js', function(){
   describe('constructor', function(){
 
-    it('should work', function(){
+    it('should initialise without arguments', function(done){
+      var imgr = new Imager();
 
+      setTimeout(function(){
+        expect(imgr.initialized).to.be.true;
+        expect(imgr.scrolled).to.be.false;
+        expect(imgr.cache).to.be.empty;
+
+        done();
+      }, 100);
     });
 
   });
