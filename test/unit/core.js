ee714463dcee9fe8679c5676ff4418b036439e9f
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-10-30 12:03:08 +0000

    Testing new `data-src` syntax.
    - initial tests are failing because the old feature is still working (good way to know the old expectation we are going to break)
    - new tests are  not working because basically it's not done yet

--- test/unit/core.js
+++ test/unit/core.js
@@ -2,6 +2,52 @@
 
 /* globals describe, beforeEach, afterEach, it, expect, Imager, jQuery, document */
 
+function loadFixtures(location){
+  var fixtures = document.createElement('div');
+  fixtures.id = 'karma-fixtures';
+  fixtures.innerHTML = window.__html__['test/fixtures/'+location+'.html'];
+  document.body.appendChild(fixtures);
+
+  return fixtures;
+}
+
 describe('Imager.js', function(){
-  
+  describe('handling {width} in data-src', function(){
+    var fixtures;
+
+    afterEach(function(){
+      if(fixtures){
+        document.body.removeChild(fixtures);
+      }
+    });
+
+    it('should does not use RegExp anymore', function(done){
+      fixtures = loadFixtures('data-src-old');
+      var imgr = new Imager({ availableWidths: [320, 640] });
+
+      setTimeout(function(){
+        Object.keys(imgr.cache).forEach(function(key){
+          var replacement = imgr.cache[key];
+
+          expect(replacement.nodeName).to.eq('IMG');
+          expect(replacement.getAttribute('src')).to.eq(replacement.getAttribute('data-src'));
+        });
+
+        done();
+      }, 100);
+    });
+
+    it('should replace {width} by the computed width or a fallback', function(done){
+      fixtures = loadFixtures('data-src-new');
+      var imgr = new Imager({ availableWidths: [320, 640] });
+
+      setTimeout(function(){
+        expect(imgr.cache['base/Demo - Grunt/Assets/Images/Generated/C-320.jpg'].getAttribute('data-src')).to.eq('base/Demo - Grunt/Assets/Images/Generated/C-{width}.jpg');
+        expect(imgr.cache['base/Demo - Grunt/Assets/Images/Generated/B-640.jpg'].getAttribute('data-src')).to.eq('base/Demo - Grunt/Assets/Images/Generated/B-{width}.jpg');
+        expect(imgr.cache['base/test/fixtures/media-320/fillmurray.jpg'].getAttribute('data-src')).to.eq('base/test/fixtures/media-{width}/fillmurray.jpg');
+
+        done();
+      }, 100);
+    });
+  });
 });
