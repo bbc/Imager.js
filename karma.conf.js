89d035e0578592a480f032ef99bb274f9556338c
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-10-31 14:42:12 +0000

    Pixel ratio tests

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
 
     // frameworks to use
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai', 'sinon'],
 
 
     // list of files / patterns to load in the browser
