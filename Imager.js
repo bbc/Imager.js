0fe88140e9a77f6442e9fc0f88e93ef4e5a548cf
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-03-19 18:32:51 +0000

    Passing the pixelRatio as a second argument of the `widthInterpolator`.

--- Imager.js
+++ Imager.js
@@ -351,7 +351,7 @@
             i   = widths.length;
 
         while (i--) {
-            map[widths[i]] = interpolator(widths[i]);
+	    map[widths[i]] = interpolator(widths[i], this.devicePixelRatio);
         }
 
         return map;
